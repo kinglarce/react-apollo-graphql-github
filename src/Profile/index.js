64fff8e5149235d647d2f964aa4689c7149d33c9
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-04 15:27:04 +0900

    Add GQL for getting current user

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const Profile = () =>
-  <div>Profile</div>
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+`;
+
+const Profile = () => (
+  <Query query={GET_CURRENT_USER}>
+    {() => <div>My Profile</div>}
+  </Query>
+);
 
 export default Profile;
