35c83f3eec2ec13929e2d7dc6bd20db942bee097
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-04 21:05:58 +0900

    Change profile component from render props to HOC

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import { GET_REPOSITORIES_OF_CURRENT_USER } from './gql';
 
 import Loading from '../Loading';
 import RepositoryList from '../Repository';
 import ErrorMessage from '../Error';
 
-const Profile = () => (
-  <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-    {({ data, loading, error }) => {
-      if (error) {
-        return <ErrorMessage error={error} />;
-      }
+const Profile = ({ data, loading, error }) => {
+  if (error) {
+    return <ErrorMessage error={error} />;
+  }
 
-      const { viewer } = data;
+  const { viewer } = data;
 
-      if (loading || !viewer) {
-        return <Loading />;
-      }
+  if (loading || !viewer) {
+    return <Loading />;
+  }
 
-      return <RepositoryList repositories={viewer.repositories} />;
-    }}
-  </Query>
-);
+  return <RepositoryList repositories={viewer.repositories} />;
+};
 
-export default Profile;
+
+export default graphql(GET_REPOSITORIES_OF_CURRENT_USER)(Profile);
