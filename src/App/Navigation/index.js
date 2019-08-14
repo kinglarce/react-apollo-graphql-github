415ec7159f0d6f8418bdf12e2947571ff0cb321c
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-14 13:21:24 +0900

    Make navigation stateless and move up setting state

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 import * as routes from '../../constants/routes';
@@ -6,28 +6,26 @@ import OrganizationSearch from '../../Organization/OrganizationSearch';
 
 import './style.css';
 
-const Navigation = ({ location: { pathname } }) => {
-  const [organizationName, setOrganizationName] = useState(
-    'facebook',
-  );
+const Navigation = ({
+  location: { pathname },
+  organizationName,
+  setOrganizationName,
+}) => (
+  <header className="Navigation">
+    <div className="Navigation-link">
+      <Link to={routes.PROFILE}>Profile</Link>
+    </div>
+    <div className="Navigation-link">
+      <Link to={routes.ORGANIZATION}>Organization</Link>
+    </div>
 
-  return (
-    <header className="Navigation">
-      <div className="Navigation-link">
-        <Link to={routes.PROFILE}>Profile</Link>
-      </div>
-      <div className="Navigation-link">
-        <Link to={routes.ORGANIZATION}>Organization</Link>
-      </div>
-
-      {pathname === routes.ORGANIZATION && (
-        <OrganizationSearch
-          organizationName={organizationName}
-          onOrganizationSearch={setOrganizationName}
-        />
-      )}
-    </header>
-  );
-};
+    {pathname === routes.ORGANIZATION && (
+      <OrganizationSearch
+        organizationName={organizationName}
+        onOrganizationSearch={setOrganizationName}
+      />
+    )}
+  </header>
+);
 
 export default withRouter(Navigation);
--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './Navigation';
@@ -9,39 +9,46 @@ import * as routes from '../constants/routes';
 
 import './style.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navigation />
+const App = () => {
+  const [organizationName, setOrganizationName] = useState(
+    'facebook',
+  );
 
-          <div className="App-main">
-            <Route
-              exact
-              path={routes.ORGANIZATION}
-              component={() => (
-                <div className="App-content_large-header">
-                  <Organization
-                    organizationName={'facebook'}
-                  />
-                </div>
-              )}
-            />
-            <Route
-              exact
-              path={routes.PROFILE}
-              component={() => (
-                <div className="App-content_small-header">
-                  <Profile />
-                </div>
-              )}
-            />
-          </div>
+  const onOrganizationSearch = useCallback((searched) => {
+    setOrganizationName(searched);
+  }, [])
+
+  return (
+    <Router>
+      <div className="App">
+        <Navigation
+          organizationName={organizationName}
+          setOrganizationName={onOrganizationSearch}
+        />
+
+        <div className="App-main">
+          <Route
+            exact
+            path={routes.ORGANIZATION}
+            component={() => (
+              <div className="App-content_large-header">
+                <Organization organizationName={organizationName} />
+              </div>
+            )}
+          />
+          <Route
+            exact
+            path={routes.PROFILE}
+            component={() => (
+              <div className="App-content_small-header">
+                <Profile />
+              </div>
+            )}
+          />
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
