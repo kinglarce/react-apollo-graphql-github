43ac29b464306b9877882f5552f9cb976f146103
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-14 12:25:00 +0900

    Add GQL query for organization

--- src/Organization/index.js
+++ src/Organization/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Organization = () => <div>Organization</div>;
+import { Query } from 'react-apollo';
+import { GET_REPOSITORIES_OF_ORGANIZATION } from './queries';
+
+const Organization = ({ organizationName }) => (
+  <Query
+    query={GET_REPOSITORIES_OF_ORGANIZATION}
+    variables={{
+      organizationName,
+    }}
+    skip={organizationName === ''}
+    notifyOnNetworkStatusChange={true}
+  >
+    {({ data, loading, error, fetchMore }) => {
+      console.log('the org data : ', { data, loading, error, fetchMore });
+    }}
+  </Query>
+);
 
 export default Organization;
